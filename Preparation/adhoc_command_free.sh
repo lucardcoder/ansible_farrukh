af8a0a8c78c1cc1cd14ebc042dca88d67571b441
lucardcoder <gokalpkocer@gmail.com>
2022-11-11 00:55:30 +0000

    .

--- Preparation/adhoc_command_free.sh
+++ Preparation/adhoc_command_free.sh
@@ -11,3 +11,10 @@ ansible localhost -m command -a " sudo yum install httpd -y"
 
 # start webserver
 ansible localhost -m command -a " sudo systemctl start httpd"
+
+#check users
+ansible localhost -m command -a "w"
+
+#check disk
+
+ansible localhost -m command -a "df -h"
