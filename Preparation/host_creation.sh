0265baf0c84141ee218ed29d748caa02ed7385cc
lucardcoder <gokalpkocer@gmail.com>
2022-11-11 02:15:57 +0000

    .

---
+++ Preparation/host_creation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash -c "$(curl https://bucket-to-check-aws-tasks.s3.amazonaws.com/AWS/scripts/shared_scripts/ansible_menu.sh)"
--- Preparation/prepare.sh
+++ Preparation/prepare.sh
@@ -1,5 +1,3 @@
-bash -c "$(curl https://bucket-to-check-aws-tasks.s3.amazonaws.com/AWS/scripts/shared_scripts/ansible_menu.sh)"
-
 
 #Creating config with parameters to automate to accept hostkeychecking in each machine
 
