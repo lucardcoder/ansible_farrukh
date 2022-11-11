5c667b159f940dc067573bf78b553bbf70ebd2c6
lucardcoder <gokalpkocer@gmail.com>
2022-11-11 00:32:37 +0000

    changes

--- adhoc_command_free.sh
+++ adhoc_command_free.sh
@@ -2,3 +2,12 @@
 
 #check memory
 ansible localhost -m command -a "free -m"
+
+# add user bob
+ansible localhost -m command -a " sudo useradd bob"
+
+# add package
+ansible localhost -m command -a " sudo yum install httpd -y"
+
+# start webserver
+ansible localhost -m command -a " sudo systemctl start httpd"
