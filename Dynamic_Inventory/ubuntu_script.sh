e38622f17225c8aa7669ecd54d8acf245709e76e
lucardcoder <gokalpkocer@gmail.com>
2023-01-10 19:25:20 +0000

    .

---
+++ Dynamic_Inventory/ubuntu_script.sh
@@ -0,0 +1,4 @@
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
+sudo apt-get install python3-pip
--- Preparation/install_ansible.sh
+++ Preparation/install_ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo amazon-linux-extras install ansible2 -y
-sudo yum install ansible-2.9.23 -yum
+sudo apt-get install ansible-2.9.23 -yum
 ansible --version
 ansible localhost -m ping
 
